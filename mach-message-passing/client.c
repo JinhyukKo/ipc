c1d0dba459779d263b53c235fa2d33c973e927fb
JinHyukKo <112324054+JinhyukKo@users.noreply.github.com>
2025-02-06 23:02:05 +0100

    Update client.c

--- mach-message-passing/client.c
+++ mach-message-passing/client.c
@@ -6,60 +6,52 @@
 
 struct {
     mach_msg_header_t header;
-    char some_text[20];
+    char some_text[100];
     char sender[20];
-    mach_msg_trailer_t trailer;
 } message;
-
 int main() {
 
-    // Create a new port.
+    // Lookup the receiver port using the bootstrap server.
     mach_port_t port;
-    kern_return_t kr = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_RECEIVE, &port);
+    kern_return_t kr = bootstrap_look_up(bootstrap_port, "com.example", &port);
     if (kr != KERN_SUCCESS) {
-        printf("mach_port_allocate() failed with code 0x%x\n", kr);
+        printf("bootstrap_look_up() failed with code 0x%x\n", kr);
         return 1;
     }
-    printf("mach_port_allocate() created port right name %d\n", port);
+    printf("bootstrap_look_up() returned port right name %d\n", port);
 
 
-    // Give us a send right to this port, in addition to the receive right.
-    kr = mach_port_insert_right(mach_task_self(), port, port, MACH_MSG_TYPE_MAKE_SEND);
-    if (kr != KERN_SUCCESS) {
-        printf("mach_port_insert_right() failed with code 0x%x\n", kr);
-        return 1;
-    }
-    printf("mach_port_insert_right() inserted a send right\n");
+    // Construct our message.
 
 
-    // Send the send right to the bootstrap server, so that it can be looked up by other processes.
-    kr = bootstrap_register(bootstrap_port, "com.example", port);
-    if (kr != KERN_SUCCESS) {
-        printf("bootstrap_register() failed with code 0x%x\n", kr);
-        return 1;
-    }
-    printf("bootstrap_register()'ed our port\n");
+    message.header.msgh_bits = MACH_MSGH_BITS(MACH_MSG_TYPE_COPY_SEND, 0);
+    message.header.msgh_remote_port = port;
+    message.header.msgh_local_port = MACH_PORT_NULL;
+    printf("sender name: ");
+    scanf("%s", message.sender);
+    getchar();
 
 
-    // Wait for a message.
+
+    // Send the message.
     while(1){
+        printf("sending message to port %d : ", message.header.msgh_remote_port);
+        fgets(message.some_text, sizeof(message.some_text), stdin);
+        kr = mach_msg(
+            &message.header,  // Same as (mach_msg_header_t *) &message.
+            MACH_SEND_MSG,    // Options. We're sending a message.
+            sizeof(message),  // Size of the message being sent.
+            0,                // Size of the buffer for receiving.
+            MACH_PORT_NULL,   // A port to receive a message on, if receiving.
+            MACH_MSG_TIMEOUT_NONE,
+            MACH_PORT_NULL    // Port for the kernel to send notifications about this message to.
+        );
 
-    kr = mach_msg(
-        &message.header,  // Same as (mach_msg_header_t *) &message.
-        MACH_RCV_MSG,     // Options. We're receiving a message.
-        0,                // Size of the message being sent, if sending.
-        sizeof(message),  // Size of the buffer for receiving.
-        port,             // The port to receive a message on.
-        MACH_MSG_TIMEOUT_NONE,
-        MACH_PORT_NULL    // Port for the kernel to send notifications about this message to.
-    );
-    if (kr != KERN_SUCCESS) {
-        printf("mach_msg() failed with code 0x%x\n", kr);
-        return 1;
-    }
-    printf("%s : ", message.sender);
-
-    message.some_text[9] = 0;
-    printf("%s \n", message.some_text);
+        if (kr != KERN_SUCCESS) {
+            printf("mach_msg() failed with code 0x%x\n", kr);
+            return 1;
+        }
+        printf("------Sent-------\n");
     }
 }
+
