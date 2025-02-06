55904e8cf3048d471275820ef8dbba7e78d9a181
JinHyukKo <112324054+JinhyukKo@users.noreply.github.com>
2025-02-06 22:30:48 +0100

    Create server.c

---
+++ mach-message-passing/server.c
@@ -0,0 +1,63 @@
+// File: receiver.c
+
+#include <stdio.h>
+
+#include <mach/mach.h>
+#include <servers/bootstrap.h>
+
+int main() {
+
+    // Create a new port.
+    mach_port_t port;
+    kern_return_t kr = mach_port_allocate(mach_task_self(), MACH_PORT_RIGHT_RECEIVE, &port);
+    if (kr != KERN_SUCCESS) {
+        printf("mach_port_allocate() failed with code 0x%x\n", kr);
+        return 1;
+    }
+    printf("mach_port_allocate() created port right name %d\n", port);
+
+
+    // Give us a send right to this port, in addition to the receive right.
+    kr = mach_port_insert_right(mach_task_self(), port, port, MACH_MSG_TYPE_MAKE_SEND);
+    if (kr != KERN_SUCCESS) {
+        printf("mach_port_insert_right() failed with code 0x%x\n", kr);
+        return 1;
+    }
+    printf("mach_port_insert_right() inserted a send right\n");
+
+
+    // Send the send right to the bootstrap server, so that it can be looked up by other processes.
+    kr = bootstrap_register(bootstrap_port, "com.example", port);
+    if (kr != KERN_SUCCESS) {
+        printf("bootstrap_register() failed with code 0x%x\n", kr);
+        return 1;
+    }
+    printf("bootstrap_register()'ed our port\n");
+
+
+    // Wait for a message.
+    struct {
+        mach_msg_header_t header;
+        char some_text[10];
+        int some_number;
+        mach_msg_trailer_t trailer;
+    } message;
+
+    kr = mach_msg(
+        &message.header,  // Same as (mach_msg_header_t *) &message.
+        MACH_RCV_MSG,     // Options. We're receiving a message.
+        0,                // Size of the message being sent, if sending.
+        sizeof(message),  // Size of the buffer for receiving.
+        port,             // The port to receive a message on.
+        MACH_MSG_TIMEOUT_NONE,
+        MACH_PORT_NULL    // Port for the kernel to send notifications about this message to.
+    );
+    if (kr != KERN_SUCCESS) {
+        printf("mach_msg() failed with code 0x%x\n", kr);
+        return 1;
+    }
+    printf("Got a message\n");
+
+    message.some_text[9] = 0;
+    printf("Text: %s, number: %d\n", message.some_text, message.some_number);
+}
